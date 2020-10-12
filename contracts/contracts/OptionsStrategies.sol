be2ad66be80b16748306f72e12a14f82d28933b6
andrekorol <anrobits@yahoo.com.br>
2020-10-12 18:22:10 -0300

    Add contract that will implement the options strategies (straddle, strangle)

---
+++ contracts/contracts/OptionsStrategies.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "./OpynV2Helpers.sol";
+
+contract OptionsStrategies is OpynV2Helpers {
+    constructor(address _OpynV2AddressBook) OpynV2Helpers(_OpynV2AddressBook) {}
+}
