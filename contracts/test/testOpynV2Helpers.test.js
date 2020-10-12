4709339588c2802592c346f0b3f364005269586d
andrekorol <anrobits@yahoo.com.br>
2020-10-11 22:32:34 -0300

    Update test

--- contracts/test/testOpynV2Helpers.test.js
+++ contracts/test/testOpynV2Helpers.test.js
@@ -10,8 +10,10 @@ contract("OpynV2Helpers", (accounts) => {
 
   describe("testing the getOpynV2Controller function", async () => {
     it("should return the address found on the documentation", async () => {
+      const opynV2ControllerAddress =
+        "0x5faCA6DF39c897802d752DfCb8c02Ea6959245Fc";
       expect(await opynV2Helpers.getOpynV2Controller()).equal(
-        "0x5faCA6DF39c897802d752DfCb8c02Ea6959245Fc"
+        opynV2ControllerAddress
       );
     });
   });
