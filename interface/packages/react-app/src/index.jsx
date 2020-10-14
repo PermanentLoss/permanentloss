f05a301fc0566151796d08c1b4279c470194d36e
andrekorol <anrobits@yahoo.com.br>
2020-10-14 18:30:40 -0300

    Change file extensions: .js -> .jsx

--- interface/packages/react-app/src/index.jsx
+++ interface/packages/react-app/src/index.jsx
@@ -1,9 +1,9 @@
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 // This is the official Uniswap v2 subgraph. You can replace it with your own, if you need to.
 // See all subgraphs: https://thegraph.com/explorer/
