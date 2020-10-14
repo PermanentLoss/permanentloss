e3ffd9f25347536a12350ad5593c15d55696b863
andrekorol <anrobits@yahoo.com.br>
2020-10-14 02:03:07 -0300

    Organize components

---
+++ interface/packages/react-app/src/components/WalletButton/WalletButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from '../../components';
+import { logoutOfWeb3Modal } from '../../utils/web3Modal';
+
+
+export default function WalletButton({ provider, loadWeb3Modal }) {
+  return (
+    <Button
+      onClick={() => {
+        if (!provider) {
+          loadWeb3Modal();
+        } else {
+          logoutOfWeb3Modal();
+        }
+      }}
+    >
+      {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+    </Button>
+  );
+}
