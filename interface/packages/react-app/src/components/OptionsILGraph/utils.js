f529048807c8074073f1aec3565f6eae38785202
andrekorol <anrobits@yahoo.com.br>
2020-10-14 21:11:49 -0300

    Fix providers and addresses

--- interface/packages/react-app/src/components/OptionsILGraph/utils.js
+++ interface/packages/react-app/src/components/OptionsILGraph/utils.js
@@ -4,8 +4,8 @@ import { Contract } from 'ethers';
 import { optionsContracts } from '../../stubs/optionsContractsGraphQl';
 import ethPriceFeed from '../../utils/ethPriceFeed';
 
-const WETH_CONTRACT = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-const USDC_CONTRACT = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const WETH_CONTRACT = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const USDC_CONTRACT = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 
 function isEpochInFuture(epoch) {
   return epoch > new Date().getTime() / 1000;
