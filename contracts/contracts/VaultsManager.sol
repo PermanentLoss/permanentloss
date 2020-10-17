e0bc1265e6ea01fd59997e93b2a124af765fe15c
andrekorol <anrobits@yahoo.com.br>
2020-10-16 23:18:35 -0300

    Add functions to deposit and withdraw collateral from MarginVaults

--- contracts/contracts/VaultsManager.sol
+++ contracts/contracts/VaultsManager.sol
@@ -4,26 +4,47 @@ pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./OpynV2Helpers.sol";
 import {IController, Actions} from "./interfaces/IController.sol";
 
 contract VaultsManager is OpynV2Helpers {
+    using SafeMath for uint256;
+
     address[3] public validCollateral;
-    mapping(address => mapping(uint256 => uint256)) public ownerVaultIdBalance;
+    IController public controller;
+    mapping(address => mapping(uint256 => uint256))[3]
+        public ownerVaultIdBalance;
 
     enum Collateral {USDC, cUSDC, WETH}
 
     event CollateralizedVaultOpened(
         address accountOwner,
+        address collateral,
         uint256 vaultId,
-        uint256 amountDeposited
+        uint256 amount
+    );
+
+    event CollateralDeposited(
+        address accountOwner,
+        address collateral,
+        uint256 vaultId,
+        uint256 amount
+    );
+
+    event CollateralWithdrawn(
+        address accountOwner,
+        address collateral,
+        uint256 vaultId,
+        uint256 amount
     );
 
     constructor(address _OpynV2AddressBook, address[3] memory _validCollateral)
         OpynV2Helpers(_OpynV2AddressBook)
     {
         validCollateral = _validCollateral;
+        controller = IController(OpynV2AddressBook.getController());
 
         address MarginPool = OpynV2AddressBook.getMarginPool();
         IERC20 collateral;
@@ -50,21 +71,10 @@ contract VaultsManager is OpynV2Helpers {
         }
     }
 
-    function openCollateralizedVault(
-        Collateral _assetToDeposit,
-        uint256 _amount
-    ) public {
-        IController controller = IController(OpynV2AddressBook.getController());
-
-        require(
-            !controller.systemFullyPaused(),
-            "VaultsManager: The Opyn V2 system is currently fully paused"
-        );
-
-        uint256 newVaultId = controller.getAccountVaultCounter(address(this)) +
-            1;
-
-        IERC20 collateral = IERC20(validCollateral[uint256(_assetToDeposit)]);
+    function _transferCollateral(Collateral _assetToTransfer, uint256 _amount)
+        internal
+    {
+        IERC20 collateral = IERC20(validCollateral[uint256(_assetToTransfer)]);
 
         require(
             collateral.balanceOf(msg.sender) >= _amount,
@@ -84,9 +94,30 @@ contract VaultsManager is OpynV2Helpers {
             transferSuccess,
             "VaultsManager: Failed transfering collateral"
         );
+    }
 
-        ownerVaultIdBalance[msg.sender][newVaultId] = _amount;
-        emit CollateralizedVaultOpened(msg.sender, newVaultId, _amount);
+    function openCollateralizedVault(
+        Collateral _assetToDeposit,
+        uint256 _amount
+    ) public {
+        require(
+            !controller.systemFullyPaused(),
+            "VaultsManager: The Opyn V2 system is currently fully paused"
+        );
+
+        uint256 newVaultId = controller.getAccountVaultCounter(address(this)) +
+            1;
+
+        _transferCollateral(_assetToDeposit, _amount);
+
+        ownerVaultIdBalance[uint256(_assetToDeposit)][msg
+            .sender][newVaultId] = _amount;
+        emit CollateralizedVaultOpened(
+            msg.sender,
+            validCollateral[uint256(_assetToDeposit)],
+            newVaultId,
+            _amount
+        );
 
         Actions.ActionArgs[] memory actions = new Actions.ActionArgs[](2);
 
@@ -107,6 +138,76 @@ contract VaultsManager is OpynV2Helpers {
             secondAddress: address(this),
             asset: validCollateral[uint256(_assetToDeposit)],
             vaultId: newVaultId,
+            amount: _amount,
+            index: 0,
+            data: "0x0000000000000000000000000000000000000000"
+        });
+
+        controller.operate(actions);
+    }
+
+    function depositCollateral(
+        Collateral _collateralToDeposit,
+        uint256 _vaultId,
+        uint256 _amount
+    ) public {
+        _transferCollateral(_collateralToDeposit, _amount);
+
+        ownerVaultIdBalance[uint256(_collateralToDeposit)][msg
+            .sender][_vaultId] = ownerVaultIdBalance[uint256(
+            _collateralToDeposit
+        )][msg.sender][_vaultId]
+            .add(_amount);
+
+        emit CollateralDeposited(
+            msg.sender,
+            validCollateral[uint256(_collateralToDeposit)],
+            _vaultId,
+            _amount
+        );
+
+        Actions.ActionArgs[] memory actions = new Actions.ActionArgs[](1);
+
+        actions[0] = Actions.ActionArgs({
+            actionType: Actions.ActionType.DepositCollateral,
+            owner: address(this),
+            secondAddress: address(this),
+            asset: validCollateral[uint256(_collateralToDeposit)],
+            vaultId: _vaultId,
+            amount: _amount,
+            index: 0,
+            data: "0x0000000000000000000000000000000000000000"
+        });
+
+        controller.operate(actions);
+    }
+
+    function withdrawCollateral(
+        Collateral _collateralToWithdraw,
+        uint256 _vaultId,
+        uint256 _amount
+    ) public {
+        ownerVaultIdBalance[uint256(_collateralToWithdraw)][msg
+            .sender][_vaultId] = ownerVaultIdBalance[uint256(
+            _collateralToWithdraw
+        )][msg.sender][_vaultId]
+            .sub(_amount);
+
+        emit CollateralWithdrawn(
+            msg.sender,
+            validCollateral[uint256(_collateralToWithdraw)],
+            _vaultId,
+            _amount
+        );
+
+        Actions.ActionArgs[] memory actions = new Actions.ActionArgs[](1);
+
+        actions[0] = Actions.ActionArgs({
+            actionType: Actions.ActionType.WithdrawCollateral,
+            owner: address(this),
+            secondAddress: msg.sender,
+            asset: validCollateral[uint256(_collateralToWithdraw)],
+            vaultId: _vaultId,
             amount: _amount,
             index: 0,
             data: "0x0000000000000000000000000000000000000000"
---
+++ contracts/utils/collateral.js
@@ -0,0 +1,7 @@
+const USDC = "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b";
+const cUSDC = "0x5B281A6DdA0B271e91ae35DE655Ad301C976edb1";
+const WETH = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
+
+const validCollateral = [USDC, cUSDC, WETH];
+
+exports.validCollateral = validCollateral;
