a36b2ef8d4a258782e7567af3f619049f5ec9549
andrekorol <anrobits@yahoo.com.br>
2020-10-18 18:49:48 -0300

    Fix input width issue, mentioned on #21
    Used styled-components instead of CSS files

--- interface/packages/react-app/src/components/PortfolioDetector/PortfolioDetector.jsx
+++ interface/packages/react-app/src/components/PortfolioDetector/PortfolioDetector.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import { EtherscanProvider, Web3Provider } from '@ethersproject/providers';
 import PropTypes from 'prop-types';
-import { Web3Provider, EtherscanProvider } from '@ethersproject/providers';
+import React from 'react';
+import styled from 'styled-components';
 
-function PortfolioDetector({web3Provider, setEthPortfolioSize }) {
-    // TODO this should detect UNI pools for web3Provider wallet and auto-populate ETH size
-    function changeEthPortfolioSize(newVal) {
-        const newValAsFloat = parseFloat(newVal);
-        if (!isNaN(newValAsFloat)) {
-            setEthPortfolioSize(newValAsFloat);
-        }
+const PortfolioSection = styled.input`
+  width: 40px;
+`;
+
+function PortfolioDetector({ web3Provider, setEthPortfolioSize }) {
+  // TODO this should detect UNI pools for web3Provider wallet and auto-populate ETH size
+  function changeEthPortfolioSize(newVal) {
+    const newValAsFloat = parseFloat(newVal);
+    if (!isNaN(newValAsFloat)) {
+      setEthPortfolioSize(newValAsFloat);
     }
+  }
 
-    return (
-        <div className="portfolio-section">
-            Amount of ETH to protect:<input type="number" defaultValue="1" onChange={e => changeEthPortfolioSize(e.target.value)} />
-        </div>
-    )
+  return (
+    <div>
+      Amount of ETH to protect:
+      <PortfolioSection
+        type="number"
+        defaultValue="1"
+        onChange={(e) => changeEthPortfolioSize(e.target.value)}
+      />
+    </div>
+  );
 }
 
 PortfolioDetector.propTypes = {
-    web3Provider: PropTypes.oneOfType([
-        PropTypes.instanceOf(Web3Provider),
-        PropTypes.instanceOf(EtherscanProvider),
-      ]).isRequired,
-    setEthPortfolioSize: PropTypes.func
-  };
+  web3Provider: PropTypes.oneOfType([
+    PropTypes.instanceOf(Web3Provider),
+    PropTypes.instanceOf(EtherscanProvider),
+  ]).isRequired,
+  setEthPortfolioSize: PropTypes.func,
+};
 
-export default PortfolioDetector
+export default PortfolioDetector;
