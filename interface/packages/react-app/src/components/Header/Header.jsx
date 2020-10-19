a10588a19830a5bfee86b1f35af528deaa5c297f
andrekorol <anrobits@yahoo.com.br>
2020-10-19 02:23:44 -0300

    Fix Header PropTypes

--- interface/packages/react-app/src/components/Header/Header.jsx
+++ interface/packages/react-app/src/components/Header/Header.jsx
@@ -5,23 +5,25 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header({ children }) {
   return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand>
-          <Nav.Link href="/" className="btn-dark">
-            PermanentLoss
-          </Nav.Link>
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto" />
-        </Navbar.Collapse>
-        <Nav.Link href="/faq" className="btn-dark">Faq</Nav.Link>
-        {children}
-      </Navbar>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand>
+        <Nav.Link href="/" className="btn-dark">
+          PermanentLoss
+        </Nav.Link>
+      </Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto" />
+      </Navbar.Collapse>
+      <Nav.Link href="/faq" className="btn-dark">
+        Faq
+      </Nav.Link>
+      {children}
+    </Navbar>
   );
 }
 
 Header.propTypes = {
-  children: PropTypes.elementType,
+  children: PropTypes.object,
 };
 
 export default Header;
