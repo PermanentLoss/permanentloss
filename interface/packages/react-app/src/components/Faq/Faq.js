94e991698d8e7adc0345683955a3419052af35df
Jacob Mortensen <pokemort@gmail.com>
2020-10-19 21:45:15 -0700

    Convert ul to bootstrap accordion

--- interface/packages/react-app/src/components/Faq/Faq.js
+++ interface/packages/react-app/src/components/Faq/Faq.js
@@ -1,54 +1,91 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+
+const AccordionWide = styled(Accordion)`
+  width: 100%;
+`;
 function Faq() {
   return (
-    <ul>
-      <li>
-        <div>
-          <h1>What is this?</h1>
-        </div>
-        A tool to protect yourself from impermanent loss in Uniswap
-        Eth/Stablecoin pools. The graph shows the amount of loss for a change
-        in % of eth price (blue Questionne), put option price (green Questionne), and call
-        option price (orange Questionne).
-      </li>
-      <li>
-        <div>
-          <h1>What's impermanent loss?</h1>
-        </div>
-        Uniswap blog post{' '}
-        <a href="https://uniswap.org/docs/v2/advanced-topics/understanding-returns/">
-          on the subject
-        </a>
-      </li>
-      <li>
-        <div>
-          <h1>How do the options work?</h1>
-        </div>
-        Read more{' '}
-        <a href="https://bankless.substack.com/p/how-to-protect-your-eth-with-opyn">
-          here
-        </a>
-      </li>
-      <li>
-        <div>
-          <h1>How can I protect myself?</h1>
-        </div>
-        Purchase options to construct a{' '}
-        <a href="https://www.investopedia.com/ask/answers/05/052805.asp">
-          straddle/strangle
-        </a>{' '}
-        on the price of eth. This{' '}
-        <a href="https://www.youtube.com/watch?v=GSIlF5q4eUk">video</a> covers
-        the concept
-      </li>
-      <li>
-        <div>
-          <h1>Can I see the source code?</h1>
-        </div>
-        Yup!{' '}
-        <a href="https://github.com/PermanentLoss/permanentloss">github</a>
-      </li>
-    </ul>
+    <AccordionWide>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            What is this?
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+          A tool to protect yourself from impermanent loss in Uniswap
+          Eth/Stablecoin pools. The graph shows the amount of loss for a change
+          in % of eth price (blue Questionne), put option price (green Questionne), and call
+          option price (orange Questionne).
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            What's impermanent loss?
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>
+          Uniswap blog post{' '}
+          <a href="https://uniswap.org/docs/v2/advanced-topics/understanding-returns/">
+            on the subject
+          </a>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Card.Header} eventKey="2">
+            How do the options work?
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="2">
+          <Card.Body>
+            Read more{' '}
+            <a href="https://bankless.substack.com/p/how-to-protect-your-eth-with-opyn">
+              here
+            </a>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Card.Header} eventKey="3">
+            How can I protect myself?
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="3">
+          <Card.Body>
+            Purchase options to construct a{' '}
+            <a href="https://www.investopedia.com/ask/answers/05/052805.asp">
+              straddle/strangle
+            </a>{' '}
+            on the price of eth. This{' '}
+            <a href="https://www.youtube.com/watch?v=GSIlF5q4eUk">video</a> covers
+            the concept
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Card.Header} eventKey="4">
+            Can I see the source code?
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="4">
+          <Card.Body>
+            Yup!{' '}
+            <a href="https://github.com/PermanentLoss/permanentloss">github</a>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </AccordionWide>
   );
 }
 
