8525e792c1f43c2068baee7f7c263b5c1cac43e8
andrekorol <anrobits@yahoo.com.br>
2020-10-22 15:09:49 -0300

    Hide WalletButton, closes #55
    Hide the Connect Wallet button on the Home and Faq pages

--- interface/packages/react-app/src/components/Header/Header.jsx
+++ interface/packages/react-app/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from './logo.png';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import WalletButton from '../WalletButton';
+import logo from './logo.png';
 
 export const Image = styled.img`
   height: 65px;
@@ -13,7 +14,19 @@ const OurNavBar = styled(Navbar)`
   padding: 0;
 `;
 
-function Header({ children }) {
+function Header({ web3Provider, loadWeb3Modal }) {
+  const [hideWalletButton, setHideWalletButton] = useState(true);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/' || location.pathname === '/faq') {
+      setHideWalletButton(true);
+    } else {
+      setHideWalletButton(false);
+    }
+  }, [location]);
+
   return (
     <OurNavBar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand>
@@ -27,13 +40,16 @@ function Header({ children }) {
       <Nav.Link href="/faq" className="btn-dark">
         Faq
       </Nav.Link>
-      {children}
+      {!hideWalletButton && (
+        <WalletButton
+          web3Provider={web3Provider}
+          loadWeb3Modal={loadWeb3Modal}
+        />
+      )}
     </OurNavBar>
   );
 }
 
-Header.propTypes = {
-  children: PropTypes.object,
-};
+Header.propTypes = WalletButton.propTypes;
 
 export default Header;
